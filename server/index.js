5c51300065b6e8059be98daaf2aa0b8da2b14d25
Stephanie Wong <swongwrites@gmail.com>
2017-12-19 13:41:37 -0800

    Refactor server for deployment

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
-
 var app = express();
+
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/../dist'));
 
-app.listen(3000, function() {
-  console.log('listening on port 3000!');
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
 });
 
