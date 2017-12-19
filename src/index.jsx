f9cd161f5950740f15773fba05fbcbdad3951898
Stephanie Wong <swongwrites@gmail.com>
2017-12-19 00:27:35 -0800

    Reformat access token input field

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import { Container, Header, Image  } from 'semantic-ui-react';
+import { Container, Header, Image, Form, Button, Grid  } from 'semantic-ui-react';
 
 class App extends React.Component {
   constructor(props) {
@@ -13,7 +13,6 @@ class App extends React.Component {
   render () {
     return (
     <div>
-
       <Container text className='bodyContainer'>
         <Image src="http://cdn.allthingsnuclear.org/wp-content/uploads/2016/01/DBD-15-Figure-1-snail-front-from-www-snail-world-com.jpg" size='large' centered />
         <Header as='h1' textAlign='center' color='orange'>THE SPEEDY SNAIL</Header>
@@ -23,6 +22,22 @@ class App extends React.Component {
           To send your first email, please generate an access token with <a href="https://postmail.invotes.com/" target="_blank"> Postmail</a>. 
           You will need to enter your access token with each visit to this page. After you receive your token, please enter it here:
         </p>
+
+        <Grid columns={2} >
+          <Grid.Row>
+            <Grid.Column>
+              <Form >
+                <Form.Input  required onChange={this.handleToken}>
+                  <input placeholder='Access Token' />
+                </Form.Input>
+              </Form>            
+            </Grid.Column>
+            <Grid.Column>
+              {}
+              <Button type='submit' basic color='orange' onClick={this.updateToken}>{this.state.status}</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
 
       </Container>
     </div>)
